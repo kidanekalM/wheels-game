f247bc5984f9bce3efc769cbea2a9676100c7451
Kidanekal <akidanekal@gmail.com>
2023-05-07 00:35:26 +0900

    update rgb

--- rgb.js
+++ rgb.js
@@ -2,7 +2,9 @@ btnStop=document.getElementById('btnStop');
 div1 = document.getElementById('div1');
 div2 = document.getElementById('div2');
 
-btnStop.addEventListener('click',function () {    
+btnStop.addEventListener('click',function () { 
+    console.log(getComputedStyle(div1).backgroundColor); 
+    console.log(getComputedStyle(div2).backgroundColor);   
     if(getComputedStyle(div1).backgroundColor==getComputedStyle(div2).backgroundColor){
         alert('You Win! The colors match.');
 
